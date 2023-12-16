13828b8c8b1572a8db67fc27aaad521584566126
jithunmadhav <jithunmadhav1@gmail.com>
2023-12-16 13:35:32 +0530

    server side url updated

--- src/axios.jsx
+++ src/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:'http://localhost:4000/',
-    baseURL:'https://connect.autoaid.online/'
+    baseURL:'https://server.connect.autoaid.online/'
 
   });
 
