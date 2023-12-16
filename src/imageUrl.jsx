2d30fb82f2e033033fe7c4c3c8474d270270c393
jithunmadhav <jithunmadhav1@gmail.com>
2023-12-16 13:40:11 +0530

    server side url updated

--- src/imageUrl.jsx
+++ src/imageUrl.jsx
@@ -1,3 +1,3 @@
 // const imageUrl='http://localhost:4000/Public/images/'
-const imageUrl='https://connect.autoaid.online/public/images/'
+const imageUrl='https://server.connect.autoaid.online/public/images/'
 export default imageUrl
